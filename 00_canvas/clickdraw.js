76884007a0990e705f0019c2bc40b2d5d92dcc22
Jack Lu <jlu9@stuy.edu>
2019-01-30 14:06:04 -0500

    oof

---
+++ 00_canvas/clickdraw.js
@@ -0,0 +1,30 @@
+// retrieve node in DOM via ID
+var c = document.getElementById("slate");
+var ctx = c.getContext("2d");
+ctx.fillStyle = "#ff0000";
+//console.log(c);
+//console.log(ctx);
+
+// origin at top left
+// increasing x to right
+// increasing y down
+// ( x start, y start, x length, y length )
+//ctx.fillStyle = "#ff0000";
+//ctx.fillRect(50, 50, 100, 200);
+
+// draw rect at location
+var draw = document.getElementById("slate");
+draw.addEventListener('click', function(e){
+    console.log("CLICKe");
+    var x = e.clientX;
+    console.log('x = ' + x);
+    var y = e.clientY;
+    console.log('y = ' + y);
+    ctx.fillRect( x, y, 50, 50 );
+});
+
+// clear button
+var clear = document.getElementById("b");
+clear.addEventListener('click', function(){
+    ctx.clearRect(0,0,600,600);
+})
