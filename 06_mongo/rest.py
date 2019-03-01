650ff9d706b0e247e98eccb940f920bcea42d977
JackLu1 <jlu2025@gmail.com>
2019-03-01 07:18:49 +0000

    1

--- 06_mongo/rest.py
+++ 06_mongo/rest.py
@@ -1,14 +1,12 @@
 # api.mongodb.com/python/current/tutorial.html
 import pymongo
 
-import pprint
 
-SERVER_ADDR = "159.230.61.139"
-connection = pymongo.MongoClient(SERVER_ADDR)
-db = connection['dataset']
-collection = db.restaurants
+#SERVER_ADDR = "159.230.61.139"
+#connection = pymongo.MongoClient(SERVER_ADDR)
 
-data = db.restaurants.find({
-    "borough":"Bronx"
-    })
-pprint(data[1])
+connection = pymongo.MongoClient('mongodb://localhost:27017/')
+db = connection.dataset
+
+for a in db.restaurants.find():
+    print(a)
