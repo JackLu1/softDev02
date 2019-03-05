ebb7fc49f1307b945027f2e36605482a0ea7f0c3
JackLu1 <jlu2025@gmail.com>
2019-03-05 00:21:55 -0500

    DO server dont work

--- 06_mongo/rest.py
+++ 06_mongo/rest.py
@@ -1,17 +1,69 @@
+# Aaron Li, Jack Lu: LuLians
+# SoftDev2 pd8
+# K06 -- Yummy Mongo Py
+# 2019-02-28
+
 # api.mongodb.com/python/current/tutorial.html
 import pymongo
+import pprint
 
 
-SERVER_ADDR = "mongodb://159.230.61.139:27017"
+#SERVER_ADDR = "159.89.238.115"
+SERVER_ADDR = "127.0.0.1"
+#connection = pymongo.MongoClient('mongodb://localhost:27017/')
 connection = pymongo.MongoClient(SERVER_ADDR)
 
-#connection = pymongo.MongoClient('mongodb://localhost:27017/')
-db = connection.dataset
 
-for a in db.restaurants.find():
-    print(a)
+pp = pprint.PrettyPrinter(indent=4)
+db = connection.db
+collection = db.restaurants
+#cursor = collection.find({"borough": "Queens"})
 
-# boroughs = ['Bronx', 'Brooklyn', 'Manhattan', 'Queens', 'Staten Island']
-# data = db.restaurants.find({
-#     "borough":"Bronx"
-#     })
+
+# restaurants with specified borough
+def restInBorough(b):
+    cursor = collection.find({"borough": b})
+    for i in cursor:
+        pp.pprint(i)
+        print()
+
+def restInZip(z):
+    cursor = collection.find({"address.zipcode" : str(z) })
+    for i in cursor:
+        pp.pprint(i)
+        print()
+
+def zipGrade(z, g):
+    cursor = collection.find({
+        "address.zipcode" : str(z),
+        "grades.grade" : g
+        })
+    for i in cursor:
+        pp.pprint(i)
+        print()
+
+def zipLowScore(z, s):
+    cursor = collection.find({
+        "address.zipcode" : str(z),
+        "grades.score": {"$lt": s}
+        })
+    for i in cursor:
+        pp.pprint(i)
+        print()
+
+# what style of food
+def zipType(z, food):
+    cursor = collection.find({
+        "address.zipcode": str(z),
+        "cuisine" : food
+        })
+    for i in cursor:
+        pp.pprint(i)
+        print()
+
+# zip code
+#restInBorough("Bronx")
+#restInZip(10282)
+#zipGrade(10282, 'C')
+#zipLowScore(10282, 4)
+zipType(10282, 'American')
