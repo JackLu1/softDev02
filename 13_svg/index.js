e0b24cda03e0fd0fb65ad72d5e5531126283b2bb
Jack Lu <jlu2025@gmail.com>
2019-03-20 01:58:22 -0400

    zz

--- 13_svg/index.js
+++ 13_svg/index.js
@@ -1,19 +1,27 @@
 var data = [4, 8, 15, 16, 23, 42];
-//Select your chart.
-var chart = d3.select(".chart");
 
-//Prepare for data join.
-var bar = chart.selectAll("div");
+var width = 420,
+	barHeight = 20;
 
-//Join your data.
-var barUpdate = bar.data(data);
+var x = d3.scaleLinear()
+	.domain([0, d3.max(data)])
+	.range([0, width]);
 
-//Instantiate new elements by appending to the “enter selection.”
-var barEnter = barUpdate.enter().append("div");
+var chart = d3.select(".chart")
+	.attr("width", width)
+	.attr("height", barHeight * data.length);
 
-//Set width of each bar proportional to its data value.
-barEnter.style("width", function(d) {
-    return d * 10 + "px"; });
+var bar = chart.selectAll("g")
+	.data(data)
+	.enter().append("g")
+	.attr("transform", function(d, i) { return "translate(0," + i * barHeight + ")"; });
 
-//Label each bar.
-barEnter.text(function(d) { return d; });
+bar.append("rect")
+	.attr("width", x)
+	.attr("height", barHeight - 1);
+
+bar.append("text")
+	.attr("x", function(d) { return x(d) - 3; })
+	.attr("y", barHeight / 2)
+	.attr("dy", ".35em")
+	.text(function(d) { return d; });
