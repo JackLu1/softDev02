2f107d84262c3f1a5897290d6ce74b88f1e0dbc9
Jack Lu <jlu2025@gmail.com>
2019-04-15 20:22:30 -0400

    fin 18

--- 18_listcomp/listcomp.py
+++ 18_listcomp/listcomp.py
@@ -4,13 +4,25 @@ def triple(n):
     tri = [[a, b, c] for a in range(1,n) for b in range(a,n) for c in range(b,n) if a**2 + b**2 == c**2 and c < n]
     print(tri)
 
-def qsort(arr):
+def qsort(lst):
     '''quicksort array arr'''
     # pivot = center
-    pivot = len(arr) / 2
-    
+    #pivot = int(len(lst) / 2)
+    #a = lst[:pivot]
+    #print(a)
+    if len(lst) < 2:
+        return lst
+    else:
+        pivot = lst[int(len(lst) / 2)]
+        left = []
+        right = []
+        mid = []
+        [left.append(i) if i < pivot else right.append(i) if i > pivot else mid.append(i) for i in lst]
+        return qsort(left) + mid + qsort(right)
 
 
-qsort([3, 4, 1, 7, 5, 0, 9, 8, 2, 6])
-#triple(20)
 
+
+triple(20)
+a = qsort([3, 4, 0, 1, 7, 5, 11, 11, 0, 9, 8, 2, 6])
+print(a)
