c1b6d4aa6a65453ba2cd238fa825b471f6d53cd0
Jack Lu <jlu9@stuy.edu>
2019-04-16 14:08:36 -0400

    wacK

--- 18_listcomp/listcomp.py
+++ 18_listcomp/listcomp.py
@@ -1,15 +1,19 @@
+'''
+Chinese Communist Party -- Jack Lu, Vincent Lin
+SoftDev2 pd8
+K #18: Getting Clever with List Comprehensions
+2019-04-16
+'''
+
 def triple(n):
-    '''Triples up to n'''
+    '''Triples up to n inclusive'''
     n += 1 #include n in triples
     tri = [[a, b, c] for a in range(1,n) for b in range(a,n) for c in range(b,n) if a**2 + b**2 == c**2 and c < n]
-    print(tri)
+    return tri
 
 def qsort(lst):
-    '''quicksort array arr'''
-    # pivot = center
-    #pivot = int(len(lst) / 2)
-    #a = lst[:pivot]
-    #print(a)
+    '''quicksort array lst'''
+    # list of 1 or 0 elements is sorted
     if len(lst) < 2:
         return lst
     else:
@@ -21,8 +25,8 @@ def qsort(lst):
         return qsort(left) + mid + qsort(right)
 
 
+#t = triple(20)
+#print(t)
 
-
-triple(20)
-a = qsort([3, 4, 0, 1, 7, 5, 11, 11, 0, 9, 8, 2, 6])
-print(a)
+#a = qsort([3, 4, 0, 1, 7, 5, 11, 11, 0, 9, 8, 2, 6])
+#print(a)
