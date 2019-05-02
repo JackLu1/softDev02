c7a4c9e8e470fceef9a7223a97f4f31bf8069e02
Jack Lu <jlu9@stuy.edu>
2019-05-02 14:09:45 -0400

    e

--- 23_memoize/big_brain_fib.py
+++ 23_memoize/big_brain_fib.py
@@ -1,19 +1,39 @@
+#def memoize(f):
+#    memo = [1, 1]
+#    def helper(x):
+#        x -= 2
+#        for _ in range(x):
+#            memo.append( memo[-1] + memo[-2] )
+#        return memo[-1]
+#    return helper
+
 def memoize(f):
-    memo = [1, 1]
+    memo = {}
     def helper(x):
-        x -= 2
-        for _ in range(x):
-            memo.append( memo[-1] + memo[-2] )
-        return memo[-1]
+        if x not in memo:
+            memo[x] = f(x)
+        return memo[x]
     return helper
 
+@memoize
 def fib(n):
     if n == 0:
-        return 1
+        return 0
     elif n == 1:
         return 1
     else:
         return fib(n-1) + fib(n-2)
 
 fib = memoize(fib)
-print(fib(40))
+print(fib(41))
+#print( fib(40) )
+
+def memoize(fxn):
+    cache = {}
+    def memoized_fxn(*args):
+        if args in cache:
+            return cache[args]
+        cache[args] = fxn(*args)
+        return result
+    return memoized_fxn
+
